aae7326e6190140fc2d0acfb0f36e238c4bc451a
Anita Kumari <anitavkapari@gmail.com>
2019-08-17 00:19:17 +0530

    add dependencies and plugins and facebook login setup , login page designs

--- app/src/main/java/com/example/dell/nonstoptask/LoginScreenActivity.java
+++ app/src/main/java/com/example/dell/nonstoptask/LoginScreenActivity.java
@@ -2,15 +2,75 @@ package com.example.dell.nonstoptask;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginScreenActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenManager;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
+public class LoginScreenActivity extends AppCompatActivity {
+private LoginButton login_button;
+private  CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        login_button =findViewById(R.id.login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+        }
+    };
+    private  void loaderProfile(AccessToken newAccesesToken)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(newAccesesToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name,last_name,email_id");
+        request.setParameters(parameters);
+        request.executeAsync();
 
     }
 }
